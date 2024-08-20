d92bdda654924ce1b082964dfe0c299181f07537
Paulo Moreira <paultergust@gmail.com>
2024-08-20 16:13:49 +0100

    Add cmp and markdown

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,11 +1,11 @@
 function Color(color)
-	color = color or "kanagawa"
-	vim.cmd.colorscheme(color)
+    color = color or "kanagawa"
+    vim.cmd.colorscheme(color)
 end
 
 function BG()
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 
--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,1 +1,4 @@
 require('gitsigns').setup()
+vim.keymap.set('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+vim.keymap.set('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+
