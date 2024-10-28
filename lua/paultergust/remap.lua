db555a66e93ef0a22e38bd209adbf677a721f5bf
paultergust <prmoreira98@gmail.com>
2024-10-28 16:26:44 +0000

    Add ReIndent function
    Change 2-space indent to 4-space, for those annoying python scripts with
    2 spaces

--- lua/paultergust/remap.lua
+++ lua/paultergust/remap.lua
@@ -2,3 +2,7 @@ vim.keymap.set('n', '<leader>h', '<C-w>h')
 vim.keymap.set('n', '<leader>j', '<C-w>j')
 vim.keymap.set('n', '<leader>k', '<C-w>k')
 vim.keymap.set('n', '<leader>l', '<C-w>l')
+
+vim.api.nvim_create_user_command('ReIndent', function()
+  vim.cmd([[%s/^\( \{2}\)\+/\=repeat(' ', len(submatch(0)) * 2)]])
+end, {})
